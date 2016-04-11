78978024c67b27bf76e005ebd309c22c14e48d10
Aaron Schachter <aaronschachter@gmail.com>
2016-04-11 09:48:50 -0700

    Fetches campaigns from DS API, renders CampaignsTable

--- assets/js/components/Campaigns.js
+++ assets/js/components/Campaigns.js
@@ -1,11 +1,71 @@
 import React from 'react'
 
 export default React.createClass({
-  render() {
+  fetchData: function() {
+    fetch('https://www.dosomething.org/api/v1/campaigns?count=50')
+      .then((res) => {
+          return res.json();
+      }).then((json) => {
+        this.setState({
+          data: json.data,
+        });
+      })
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.fetchData();
+  },
+  render: function() {
     return (
-      <div>
-        Campaigns
-      </div>
+      <CampaignsTable data={this.state.data} />
     );
   }
 })
+
+var CampaignsTable = React.createClass({
+  render: function() {
+    var campaigns = this.props.data.map(function(campaign) {
+      var campaign_url = '/campaign/' + campaign.id;
+      return (
+        <CampaignsTableRow 
+          campaign={campaign}
+          key={campaign.id}
+        />
+      );
+    });
+    return (
+      <table className="table table-hover">
+        <tbody>
+        <tr>
+          <th>All Campaigns</th>
+          <th></th>
+          <th></th>
+        </tr>
+        {campaigns}
+        </tbody>
+      </table>
+    );
+  }
+});
+
+var CampaignsTableRow = React.createClass({
+  render: function() {
+    var inboxUrl = '/campaign/' + this.props.campaign.id;
+    var galleryUrl = '/gallery/' + this.props.campaign.id
+    return (
+      <tr>
+        <td>
+          {this.props.campaign.title}
+        </td>
+        <td>
+          <small>{this.props.campaign.tagline}</small>
+        </td>
+        <td>
+          <small>{this.props.campaign.status.toUpperCase()}</small>
+        </td>
+      </tr>
+    );
+  }
+});
