1f802eb9b99dece84c591ac37c814e490212bfa0
Aaron Schachter <aaronschachter@gmail.com>
2016-04-12 12:25:22 -0700

    Starts on fetching Profile activity

--- assets/js/components/Profile.js
+++ assets/js/components/Profile.js
@@ -1,6 +1,28 @@
 import React from 'react'
 
 export default React.createClass({
+  componentDidMount: function() {
+    this.fetchData();
+  },
+  fetchData: function() {
+    var url = 'https://northstar.dosomething.org/v1/signups?user=' + this.props.params.userId;
+    fetch(url)
+      .then((res) => {
+        console.log(res);
+        return res.json();
+      }).then((json) => {
+        this.setState({
+          data: json.data,
+          loaded: true,
+       });
+     })
+  },
+  getInitialState: function() {
+    return {
+      data: [],
+      loaded: false,
+    };
+  },
   render: function() {
     // @todo: Move this into a storage.users.get function
   	var userId = this.props.params.userId;
@@ -12,7 +34,17 @@ export default React.createClass({
     if (localStorage['user_' + userId + '_photo']) {
     	photo = localStorage['user_' + userId + '_photo'];
     }
-
+    var content; 
+    if (!this.state.loaded) {
+      content = <div>Loading...</div>;
+    }
+    else {
+      content = this.state.data.map(function(signup) {
+        return (
+          <div>{signup.id}</div>
+        );
+      });      
+    }
     return (
       <div className="row profile">
         <div className="col-md-3 sidebar">
@@ -28,6 +60,9 @@ export default React.createClass({
           <div className="page-header">
             <h1>{firstName}</h1>
             <h4>{'united states'.toUpperCase()}</h4>
+          </div>
+          <div className="row">
+            {content}
           </div>
         </div>
       </div>
