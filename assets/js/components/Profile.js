1b549838e87a709a2b3ddbdde67e030237c910e9
Aaron Schachter <aaronschachter@gmail.com>
2016-04-12 14:45:01 -0700

    Starts on Reportback.js

--- assets/js/components/Profile.js
+++ assets/js/components/Profile.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import Reportback from './Reportback';
 
 export default React.createClass({
   componentDidMount: function() {
@@ -8,7 +9,6 @@ export default React.createClass({
     var url = 'https://northstar.dosomething.org/v1/signups?user=' + this.props.params.userId;
     fetch(url)
       .then((res) => {
-        console.log(res);
         return res.json();
       }).then((json) => {
         this.setState({
@@ -40,30 +40,25 @@ export default React.createClass({
     }
     else {
       content = this.state.data.map(function(signup) {
-        return (
-          <div>{signup.id}</div>
-        );
+        if (signup.reportback) {
+          return <Reportback
+            campaign={signup.campaign}
+            key={signup.id} 
+            reportback={signup.reportback} 
+          />;
+        }
+        return null;
       });      
     }
     return (
-      <div className="row profile">
-        <div className="col-md-3 sidebar">
-          <div className="avatar">
-            <img src={photo} className="img-circle img-responsive center-block" />
-          </div>
-          <ul id="leftNav" className="nav nav-pills nav-stacked">
-            <li role="presentation"><a>Email</a></li>
-            <li role="presentation"><a>Report</a></li>
-          </ul>
+      <div>
+        <div className="page-header">
+          <img src={photo} className="img-circle avatar pull-left" />
+          <h1>{firstName}</h1>
+          <h4>{'united states'.toUpperCase()}</h4>
         </div>
-        <div className="col-md-9">
-          <div className="page-header">
-            <h1>{firstName}</h1>
-            <h4>{'united states'.toUpperCase()}</h4>
-          </div>
-          <div className="row">
-            {content}
-          </div>
+        <div>
+          {content}
         </div>
       </div>
     );
---
+++ assets/js/components/Reportback.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import NavLink from './NavLink'
+
+export default React.createClass({
+  render() {
+    var campaignUrl = '/campaigns/' + this.props.campaign.id;
+    var label = this.props.campaign.reportback_info.noun + ' ' + this.props.campaign.reportback_info.verb;
+    var reportbackItem = this.props.reportback.reportback_items.data[0];
+    return (
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          <h4 className="pull-right">{this.props.reportback.quantity} <small>{label}</small></h4>
+          <h4><NavLink to={campaignUrl}>{this.props.campaign.title}</NavLink></h4>
+          <small>{this.props.campaign.tagline}</small>
+        </div>
+        <div className="panel-body">
+          <Carousel
+            key={this.props.reportback.id}
+            data={this.props.reportback.reportback_items.data}
+            reportback={this.props.reportback}
+            reportbackItem={reportbackItem}
+          />
+        </div>
+      </div>
+    );
+  }
+});
+
+var Carousel = React.createClass({
+  handleClick: function(increment) {
+//    this.props.bumpIndex(increment);
+  },
+  render: function() {
+    var items = this.props.data.map(function(reportbackItem, itemIndex) {
+      return (
+        <CarouselItem 
+          reportbackItem={reportbackItem}
+          itemIndex={itemIndex}
+          key={reportbackItem.id}
+        />
+      );
+    });
+    var carouselId = 'carousel' + this.props.reportback.id.toString();
+    var controls = null;
+    if (items.length > 1) {
+      controls = this.renderControls('#' + carouselId);
+    }
+    return (
+      <div id={carouselId} className="carousel slide" data-ride="carousel" data-interval="false">
+        <div className="carousel-inner" role="listbox">
+          {items}
+          {controls}
+        </div>
+      </div>
+    );
+  },
+  renderControls: function(href) {
+    return (
+      <div>
+      <a onClick={this.handleClick.bind(this, -1)} className="left carousel-control" href={href}  role="button" data-slide="prev">
+        <span className="glyphicon glyphicon-chevron-left carousel-button" aria-hidden="true"></span>
+        <span className="sr-only">Previous</span>
+      </a>
+      <a onClick={this.handleClick.bind(this, 1)} className="right carousel-control" href={href}  role="button" data-slide="next">
+        <span className="glyphicon glyphicon-chevron-right carousel-button" aria-hidden="true"></span>
+        <span className="sr-only">Next</span>
+      </a>
+      </div>
+    );
+  }
+});
+
+var CarouselItem = React.createClass({
+  render: function() {
+    var itemClassName = 'item';
+    if (this.props.itemIndex == 0) {
+      itemClassName = itemClassName + ' active';
+    }
+    return (
+      <div className={itemClassName}>
+        <img 
+          src={this.props.reportbackItem.media.uri}
+          className="center-block"
+        />
+        <div className="carousel-caption">
+          {this.props.reportbackItem.caption}
+        </div>
+      </div>
+    );
+  }
+});
