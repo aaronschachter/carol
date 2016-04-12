9e372cfd19af4eb2db899fee2969c55904bacfd3
Aaron Schachter <aaronschachter@gmail.com>
2016-04-12 11:08:58 -0700

    Adds express, server.js per react-router-tutorial lesson 11

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
+var webpack = require('webpack')
+
 module.exports = {
   entry: './index.js',
-
-  output: {
-    filename: 'bundle.js',
-    publicPath: ''
-  },
-
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
     ]
-  }
+  },
+  output: {
+    path: 'dist',
+    filename: 'bundle.js',
+    publicPath: ''
+  },
+  plugins: process.env.NODE_ENV === 'production' ? [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  ] : [],
 }
