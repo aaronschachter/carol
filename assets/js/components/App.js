3b800b0d4833136465c6eb1455f1b84155a5f852
Aaron Schachter <aaronschachter@gmail.com>
2016-04-13 14:07:26 -0700

    Fixes spacing

--- assets/js/components/App.js
+++ assets/js/components/App.js
@@ -7,37 +7,37 @@ export default React.createClass({
   render() {
     return (
       <div>
-		<nav className="navbar navbar-default navbar-fixed-top">
-		  <div className="container">
-		    <div className="navbar-header">
-		      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-		        <span className="sr-only">Toggle navigation</span>
-		        <span className="icon-bar"></span>
-		        <span className="icon-bar"></span>
-		        <span className="icon-bar"></span>
-		      </button>
-		      <a className="navbar-brand"><img src="https://raw.githubusercontent.com/DoSomething/LetsDoThis-iOS/develop/Lets%20Do%20This/Images.xcassets/DS%20Logo.imageset/DS%20Logo.png" height="20" width="25" /></a>
-		    </div>
-		    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-		      <ul className="nav navbar-nav">
-		        <NavItem to="/campaigns">Campaigns</NavItem>
-		        <NavItem to="/members">Members</NavItem>
-		      </ul>
-		      <ul className="nav navbar-nav navbar-right">
-		        <li className="dropdown">
-		          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><small>carol@dosomething.org</small> <span className="caret"></span></a>
-		          <ul className="dropdown-menu">
-		            <li><a href="#">Reviews</a></li>
-		            <li role="separator" className="divider"></li>
-		            <li><a href="#">Logout</a></li>
-		          </ul>
-		        </li>
-		      </ul>
-		    </div>
-		  </div>
-		</nav>
-		<div className="container">
-        {this.props.children}
+    		<nav className="navbar navbar-default navbar-fixed-top">
+    		  <div className="container">
+    		    <div className="navbar-header">
+    		      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+    		        <span className="sr-only">Toggle navigation</span>
+    		        <span className="icon-bar"></span>
+    		        <span className="icon-bar"></span>
+    		        <span className="icon-bar"></span>
+    		      </button>
+    		      <a className="navbar-brand"><img src="https://raw.githubusercontent.com/DoSomething/LetsDoThis-iOS/develop/Lets%20Do%20This/Images.xcassets/DS%20Logo.imageset/DS%20Logo.png" height="20" width="25" /></a>
+    		    </div>
+    		    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    		      <ul className="nav navbar-nav">
+    		        <NavItem to="/campaigns">Campaigns</NavItem>
+    		        <NavItem to="/members">Members</NavItem>
+    		      </ul>
+    		      <ul className="nav navbar-nav navbar-right">
+    		        <li className="dropdown">
+    		          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><small>carol@dosomething.org</small> <span className="caret"></span></a>
+    		          <ul className="dropdown-menu">
+    		            <li><a href="#">Reviews</a></li>
+    		            <li role="separator" className="divider"></li>
+    		            <li><a href="#">Logout</a></li>
+    		          </ul>
+    		        </li>
+    		      </ul>
+    		    </div>
+    		  </div>
+    		</nav>
+    		<div className="container">
+          {this.props.children}
         </div>
       </div>
     )
