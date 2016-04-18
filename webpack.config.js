82e4b18d585c058d944bb1774e874b42ac749bd3
Aaron Schachter <aaronschachter@gmail.com>
2016-04-18 12:03:59 -0700

    Rename as client.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './index.js',
+  entry: './client.js',
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
